ca6c61efb69cc53a3db64b635e5d8e6caa068d91
Zalron <davidrlawson@optusnet.com.au>
2021-05-25 17:39:58 +1000

    added threading module

--- ItemTest/ItemTest/main.cpp
+++ ItemTest/ItemTest/main.cpp
@@ -1,6 +1,9 @@
 #include "flecs.h"
 #include "Item.h"
 #include "ItemConfig.h"
+#include "flecs_dash.h"
+#include "flecs_systems_civetweb.h"
+#include "flecs_os_api_stdcpp.h"
 #include <iostream>
 #include <cstdint>
 
@@ -10,7 +13,15 @@ int main(int argc, char* argv[])
 {
     cout << "RAND_MAX value is " << RAND_MAX << endl;
 
+    stdcpp_set_os_api();
+
     flecs::world world;
+
+    world.import<flecs::dash>();
+
+    world.import<flecs::systems::civetweb>();
+
+    world.entity().set<flecs::dash::Server>({ 9090 });
 
     world.entity("Item").set<ItemSpawning>({1000});
 
